1dc1268a2674d51c5ba9b4bdf01883bf7daff2ff
Fraan <franco34@live.com.ar>
2024-03-05 22:37:03 -0300

    Skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import Root from "./routes/root";
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        background: ""
+      }
+    })
+  }
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,15 @@
+export default function Root() {
+    return (
+      <>
+        <div id="navbar">
+          <h1>Novafy</h1>
+          <div>
+           asd
+          </div>
+          <nav>asd
+          
+          </nav>
+        </div>
+      </>
+    );
+  }
